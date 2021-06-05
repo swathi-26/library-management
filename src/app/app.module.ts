5245d94a2970c34a4848555db076db24dceb8fa3
Swathi <swatvenk@cisco.com>
2021-06-05 15:48:34 +0530

    Adding search, display page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBooksComponent } from './search-books/search-books.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { DisplayBooksComponent } from './display-books/display-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBooksComponent,
+    DisplayBooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
